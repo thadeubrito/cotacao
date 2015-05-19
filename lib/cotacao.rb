e95189919564c2d02cd1cbfdb504159200c84c56
Thadeu Brito <itvbrito@gmail.com>
2015-05-19 16:35:13 -0300

    Consuming the api

--- lib/cotacao.rb
+++ lib/cotacao.rb
@@ -1,9 +1,18 @@
 require "cotacao/version"
-
+require 'httparty'
+require 'json'
 
 module Cotacao
   class Cotacao
+
+    def initialize
+      apiUrl = 'http://developers.agenciaideias.com.br/cotacoes/json'
+      response = HTTParty.get(apiUrl)
+      @quotations = JSON.parse(response.body)
+    end
+
     def now
+      puts @quotations["dolar"]["cotacao"]
     end
   end
 end
