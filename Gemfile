821f33e55076d736630409994deb778ca361efe4
Thadeu Brito <itvbrito@gmail.com>
2015-05-19 21:06:47 -0300

    Show others quotations

--- Gemfile
+++ Gemfile
@@ -4,5 +4,4 @@ source 'https://rubygems.org'
 gemspec
 
 gem 'json'
-
 gem 'httparty'
--- lib/cotacao.rb
+++ lib/cotacao.rb
@@ -6,13 +6,21 @@ module Cotacao
   class Cotacao
 
     def initialize
-      apiUrl = 'http://developers.agenciaideias.com.br/cotacoes/json'
+      apiUrl   = 'http://developers.agenciaideias.com.br/cotacoes/json'
       response = HTTParty.get(apiUrl)
-      @cotacoes = JSON.parse(response.body)
+      @data     = JSON.parse(response.body)
     end
 
     def now
-      puts "Dolar: R$ #{@cotacoes['dolar']['cotacao']} - Variação: #{@cotacoes['dolar']['variacao']}"
+      console_print("Dolar: R$ #{@data['dolar']['cotacao']} - Variação: #{@data['dolar']['variacao']}")
+      console_print("Euro: R$ #{@data['euro']['cotacao']} - Variação: #{@data['euro']['variacao']}")
+      console_print("Bovespa: #{@data['bovespa']['cotacao']} - Variação: #{@data['bovespa']['variacao']}")
     end
+
+    def console_print(cotacao)
+      puts '----------------------'
+      puts cotacao
+    end
+
   end
 end
